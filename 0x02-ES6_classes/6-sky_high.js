3180475589bf763b516ccf09d81296b4b90b4cb1
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-06 14:27:21 +0000

    adding setter method for floors attribute

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -6,12 +6,12 @@ export default class SkyHighBuilding extends Building {
     this._floors = floors; // eslint-disable-line no-underscore-dangle
   }
 
-  get sqft() {
-    return this._sqft; // eslint-disable-line no-underscore-dangle
-  }
-
   get floors() {
     return this._floors; // eslint-disable-line no-underscore-dangle
+  }
+
+  set floors(value) {
+    this._floors = value; // eslint-disable-line no-underscore-dangle
   }
 
   evacuationWarningMessage() {
