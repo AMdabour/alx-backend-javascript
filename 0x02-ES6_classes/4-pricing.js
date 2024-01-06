9ad22c5f21b8017638a1fe0204a994895429f221
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-06 09:46:23 +0000

    class Pricing.............

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount; // eslint-disable-line no-underscore-dangle
+    this._currency = currency; // eslint-disable-line no-underscore-dangle
+  }
+
+  get amount() {
+    return this._amount; // eslint-disable-line no-underscore-dangle
+  }
+
+  set amount(value) {
+    this._amount = value; // eslint-disable-line no-underscore-dangle
+    return this._amount; // eslint-disable-line no-underscore-dangle
+  }
+
+  get currency() {
+    return this._currency; // eslint-disable-line no-underscore-dangle
+  }
+
+  set currency(value) {
+    this._currency = value; // eslint-disable-line no-underscore-dangle
+    return this._currency; // eslint-disable-line no-underscore-dangle
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`; // eslint-disable-line no-underscore-dangle
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
