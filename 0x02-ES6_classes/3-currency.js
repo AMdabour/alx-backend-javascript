3b32ab0888d3f7fdcc28ece25bfa7b3fdd74ae67
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-06 08:47:25 +0000

    Currency class with getter and setter methods

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,28 @@
+export default class Currency {
+  constructor(code, name) {
+    this._code = code; // eslint-disable-line no-underscore-dangle
+    this._name = name; // eslint-disable-line no-underscore-dangle
+  }
+
+  get code() {
+    return this._code; // eslint-disable-line no-underscore-dangle
+  }
+
+  set code(value) {
+    this._code = value; // eslint-disable-line no-underscore-dangle
+    return this._code; // eslint-disable-line no-underscore-dangle
+  }
+
+  get name() {
+    return this._name; // eslint-disable-line no-underscore-dangle
+  }
+
+  set name(value) {
+    this._name = value; // eslint-disable-line no-underscore-dangle
+    return this._name; // eslint-disable-line no-underscore-dangle
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`; // eslint-disable-line no-underscore-dangle
+  }
+}
