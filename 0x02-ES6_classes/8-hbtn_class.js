c32373945e7e2f60b7593614c2fc39638b6530b9
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-07 10:51:54 +0000

    casting ........

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,14 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size; // eslint-disable-line no-underscore-dangle
+    this._location = location; // eslint-disable-line no-underscore-dangle
+  }
+
+  valueOf() {
+    return this._size; // eslint-disable-line no-underscore-dangle
+  }
+
+  toString() {
+    return this._location; // eslint-disable-line no-underscore-dangle
+  }
+}
