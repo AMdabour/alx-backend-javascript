8f5895220ab1ca16dfc6d7a58586233c3beb3edf
Ahmed Mahmoud Dabour <ah910905@gmail.com>
2024-01-10 00:47:20 +0000

    mapping

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,13 @@
+export default function groceriesList() {
+  const arrayObjects = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+
+  const map = new Map(arrayObjects);
+
+  return map;
+}
