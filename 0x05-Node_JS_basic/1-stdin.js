d01aeb7f6577852e67c82bfbd3137c1280ba6d5d
a-dabour <ah910905@gmail.com>
2024-02-27 18:41:36 +0200

    edit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
-function readInput() {
-  console.log('Welcome to Holberton School, what is your name?');
-  process.stdin.setEncoding('utf-8');
-  process.stdin.on('data', (name) => {
-    console.log(`Your name is: ${name.trim()}`);
-  });
-  process.stdin.on('end', () => {
-    console.log('This important software is now closing');
-  });
-}
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
+});
